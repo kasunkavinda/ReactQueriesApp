c15df9b4af7c3a4ab6f2d0c5a632d1cd6f2db7c0
Kasun <kasunkavinda.bc@gmail.com>
2022-03-17 22:55:57 +0530

    more configurations

--- pages/rq.js
+++ pages/rq.js
@@ -7,16 +7,25 @@ const fetchSuperHeroes = () => {
 };
 
 const Rq = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const onSuccess = (data) => {
+    console.log("data fetching successed", data);
+  };
+  const onError = (error) => {
+    console.log("data fetching not successed", error);
+  };
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
       // cacheTime: 300000, //default cacheTime is 5 min
       // staleTime: 30000,  //default staleTime is 0 seconds
-      refetchOnMount: true,
-      refetchOnWindowFocus: true, //A  - diff between A, B and C is, A, B need user interation. C don't
-      // refetchInterval: 2000, //B
-      refetchIntervalInBackground: true, //C
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: true, //A  - diff between A, B and C is, A, B need user interation. C don't
+      // refetchInterval: 2000, //B // refetching data every 2 seconds //wont work when loose the window focus
+      // refetchIntervalInBackground: true, //C refetching data and will work wehn loose the window focus
+      // enabled: false, // dont fetch super hearoes when page load
+      onSuccess: onSuccess,
+      onError: onError,
     }
   );
 
@@ -25,8 +34,9 @@ const Rq = () => {
   return (
     <div>
       <h2>RQ Super Heroes</h2>
+      <button onClick={refetch}>Refetch Super Heroes</button>
       {isError && <div>{error.message}</div>}
-      {isLoading && <div>Loading.....</div>}
+      {isLoading || (isFetching && <div>Loading.....</div>)}
       {data?.data.map((hero) => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
